47a495fd6bf2dada813d119e97f2d6e806226035
yAnchalSharmaofficial <133966092+yAnchalSharmaofficial@users.noreply.github.com>
2026-04-17 15:41:39 +0530

    Update app.js --> Fixed DeployeMent issue due to undefined curruser

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next) => {
     res.locals.successMsg = req.flash("success");
     res.locals.errorMsg = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     next();
 })
 
